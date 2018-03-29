2703abb6d9d3d01507d062ceb4866e1bf4600ed5
Enrique Fernandez <efcasado@gmail.com>
2018-03-29 15:17:46 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chronex.Mixfile do
   def project do
     [
       app: :chronex,
-      version: "1.0.2",
+      version: "1.0.3",
       description: "Library to seamlessly add instrumentation to your code.",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
